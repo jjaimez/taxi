8e30c70bd253ea6e5f6a2085da73ac700cb09804
jjaimez <jacinto.riocuarto@gmail.com>
2014-10-28 13:41:35 -0300

    opiniones

---
+++ js/formOpinar.js
@@ -0,0 +1,39 @@
+<!--
+function esEntero(numero): boolean{  
+ 	if (isNaN(numero)){
+        return false;
+    } 
+    else{
+        if (numero % 1 == 0) {
+        	if (0 < numero){
+            	if (11 > numero){
+            		return true;
+            	}
+        	}
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+function validarDatos(){
+	var nombreOpinion = document.getElementById("nombreOpinion").value;
+	var preferOpinion = document.getElementById("preferOpinion").value;
+	var puntOpinion = document.getElementById("puntOpinion").value;
+	var AutosOpinion = document.getElementById("AutosOpinion").value;
+	var PrecioOpinion = document.getElementById("PrecioOpinion").value;
+	var leyOpinion = document.getElementById("leyOpinion").value;
+	var opinionOpinion = document.getElementById("opinionOpinion").value;
+	if ((nombreOpinion != "") && (opinionOpinion =! "") && (preferOpinion != "") && (puntOpinion != "") && (AutosOpinion != "") && (preferOpinion != "")
+		&& (leyOpinion != "")){
+		if ((esEntero(preferOpinion)) && (esEntero(puntOpinion)) && (esEntero(AutosOpinion)) && (esEntero(PrecioOpinion)) && (esEntero(leyOpinion))) {
+			document.getElementById("formulario").submit();
+		} else {
+			alert("Por favor complete con datos correctos");
+		}
+	} else {
+		alert("Por favor complete todos los campos");
+	}	
+}
+--> 
--- remiseria1.php
+++ remiseria1.php
@@ -2,11 +2,14 @@
 <html lang="es">
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/remiserias.css">
+		<script type="text/javascript" language="javascript" src="js/lytebox.js"></script>
+		<link rel="stylesheet" href="css/lytebox.css" type="text/css" media="screen" />
 	</head>
 	<body>
 		<div>	
 			<?php 
-					session_start();			
+					session_start();
+					error_reporting(0);			
 					$db_hostname = 'localhost';
 					$db_database = 'taxi';
 					$db_username = 'root';
@@ -14,6 +17,23 @@
 					
 					$con = mysql_connect($db_hostname,$db_username,$db_password); 
 					mysql_select_db($db_database,$con); 
+
+					if(isset($_POST['preferOpinion'])) {				
+						$preferOpinion = $_POST['preferOpinion'];
+						$puntOpinion = $_POST['puntOpinion'];
+						$AutosOpinion = $_POST['AutosOpinion'];
+						$PrecioOpinion = $_POST['PrecioOpinion'];
+						$opinionOpinion = $_POST['opinionOpinion'];
+						$nombreOpinion = $_POST['nombreOpinion'];
+						$leyOpinion = $_POST['leyOpinion'];						
+						$query = "INSERT INTO opiniones (`preferencias`,`puntualidad`,`autos`,`precio`,`ley`,`opinion`,`nombre`,`empresa`) VALUES ('$preferOpinion','$puntOpinion','$AutosOpinion','$PrecioOpinion','$leyOpinion','$opinionOpinion','$nombreOpinion','empresa ejemplo');";
+						$rec = mysql_query($query);
+						if ($rec){
+							echo "TODO PIOLA";
+						} ELSE {
+							echo mysql_error();
+						}
+					}
 
 					$query = "SELECT * FROM opiniones where empresa='empresa ejemplo';";
 					$result = mysql_query($query);
@@ -36,28 +56,30 @@
 						$nombres[$i] = $row['nombre'];
 						$i = $i+1;
 					}
-			?>		
+			?>
+			<section id="titulo">			
+			<img src="imagenes/logoramallo.jpg">
+			</section>		
 			<section id="datos">
-			<p id="titulo">REMISERIA 1</p>
-			<img src="">
+			<p id="nombreEmpresa">Remiseria 1</p>
 			<p>Telefono: 4545454</p>
 			<p>Direccion: Ejemplo 2104 (Banda Norte)</p>
+			<p><a href="formOpinar.php?empresa=empresaejemplo" class="lytebox"><input type="button" id="btnOpinar" value="Opinar"></a></p>	
 			</section>
 			<section id="puntajes">
 			<p>Preferencias: <?php echo ($preferencias/$filas);?></p>	
 			<p>Puntualidad: <?php echo ($puntualidad/$filas);?></p>	
 			<p>Autos: <?php echo ($autos/$filas);?></p>	
 			<p>Precio: <?php echo ($precio/$filas);?></p>	
-			<p>Respeto de las leyes de transito: <?php echo ($leyes/$filas);?></p>	
-			</section>
-			<p><input type="button" value="Opinar"></p>
+			<p>Respeto de las leyes de transito: <?php echo ($leyes/$filas);?></p>			
+			</section>			
 			<section id="opinones">
-			<p>Opiniones:</p>
+			<p id="nombreEmpresa">Opiniones:</p>
 			<?php
-				for ($i = 0; $i < $count = count($opiniones); $i++) {
+				for ($i = count($opiniones)-1; $i >= 0; $i--) {
 					$aux = $opiniones[$i];
 					$aux2 = $nombres[$i];
-					echo "<p>Nombre: ".$aux2."</p>";
+					echo "<p>".$aux2.":</p>";
 					echo "<p>".$aux."</p>";
 				}	
 			?>
